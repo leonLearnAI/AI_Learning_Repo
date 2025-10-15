7f6664bcab27c5933f606b7689bc4d075f60b45a
Leon <leon.ai.develop@gmail.com>
2025-10-15 14:59:09 +0100

    Rename Folders

--- 01_Foundation_Python/09_Slice_Operation.py
+++ 01_Foundation_Python/09_Slice_Operation.py
@@ -1,4 +1,4 @@
-str = "万过薪月,员序程马黑来,nohtyP来"
+str = "hello, world, come on, it's me"
 # str1 = str[5:10]
 # print(str1)
 # str2 = str1[::-1]
@@ -7,7 +7,7 @@ str = "万过薪月,员序程马黑来,nohtyP来"
 str_list = str.split(",")
 str1 = str_list[1]
 print(str1)
-str2 = str1.replace("来","")
+str2 = str1.replace("come","")
 print(str2)
 str3 = str2[::-1]
 print(str3)
