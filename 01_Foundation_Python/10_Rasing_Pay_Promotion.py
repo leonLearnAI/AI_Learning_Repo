0b527f445fd26c95c255476371161fca50701428
Leon <leon.ai.develop@gmail.com>
2025-10-15 14:54:40 +0100

    translate the words

--- 01_Foundation_Python/10_Rasing_Pay_Promotion.py
+++ 01_Foundation_Python/10_Rasing_Pay_Promotion.py
@@ -1,32 +1,32 @@
 import time
 
 worker_list = {
-    "王力鸿" : {
-        "部门":"科技部",
-        "工资":3000,
-        "级别":1
-    },"周杰轮" : {
-        "部门":"市场部",
-        "工资":5000,
-        "级别":2
-    },"林俊节" : {
-        "部门":"市场部",
-        "工资":7000,
-        "级别":3
-    },"张学油" : {
-        "部门":"科技部",
-        "工资":4000,
-        "级别":1
-    },"刘德滑" : {
-        "部门":"市场部",
-        "工资":6000,
-        "级别":2
+    "Lihong" : {
+        "Department":"Technology",
+        "Salary":3000,
+        "Level":1
+    },"Jay" : {
+        "Department":"Marketing",
+        "Salary":5000,
+        "Level":2
+    },"Jenny" : {
+        "D":"Marketing",
+        "Salary":7000,
+        "Level":3
+    },"Xueyou" : {
+        "Department":"Technology",
+        "Salary":4000,
+        "Level":1
+    },"Dehua" : {
+        "Department":"Marketing",
+        "Salary":6000,
+        "Level":2
     }
 }
-print(f"调级之前：{worker_list}")
+print(f"Before promotion: {worker_list}")
 for k in worker_list:
     info_list = worker_list[k]
-    if int(info_list["级别"]) == 1:
-        worker_list[k]["工资"] += 1000
-        worker_list[k]["级别"] += 1
-print(f"调级之后：{worker_list}")
+    if int(info_list["Level"]) == 1:
+        worker_list[k]["Salary"] += 1000
+        worker_list[k]["Level"] += 1
+print(f"After promotion: {worker_list}")
--- 01_Foundation_Python/12_Receipt.py
+++ 01_Foundation_Python/12_Receipt.py
@@ -5,7 +5,7 @@ with open("bill_file.txt", "w", encoding = "UTF-8") as new_file:
     for bill in bill_list:
         bill_str = bill.strip()
         bill_filed = bill_str.split(",")
-        print(f"the new bill is：{bill_filed}, the type of the bill is {type(bill_filed)}")
+        print(f"the new bill is: {bill_filed}, the type of the bill is {type(bill_filed)}")
         if bill_filed[0] == "name" or "formal" in bill_filed[-1]:
            new_file.write(",".join(bill_filed) + "\n")
 
